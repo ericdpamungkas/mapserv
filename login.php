f029bef9c5ca1565a58f8c8b1ac773015853706f
Dea Ditawijaya <deyaabew@gmail.com>
2016-01-31 22:47:51 +0700

    Update login.php
    Update pop-up

--- login.php
+++ login.php
@@ -41,11 +41,11 @@ function cek(){
 	var user= document.getElementById('userid').value;
 	var pass= document.getElementById('passwd').value;
 	if(user.replace(/^\s+|\s+$/g, '')==''){
-		alert('Maaf, Username Anda masih kosong !');
+		alert('Username tidak boleh kosong !');
 		return false;
 	} 
 	if(pass.replace(/^\s+|\s+$/g, '')==''){
-		alert('Maaf, Password Anda masih kosong  !');
+		alert('Password tidak boleh kosong !');
 		return false;
 	}
 	return true;
